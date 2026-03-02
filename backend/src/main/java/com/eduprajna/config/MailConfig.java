0523f934ee4ca7c8377cfa31454b53330ce070e4
Keerthudarshu <Keerthudarshu06@gmail.com>
2026-03-02 12:40:27 +0530

    mailconfig solved

--- backend/src/main/java/com/eduprajna/config/MailConfig.java
+++ backend/src/main/java/com/eduprajna/config/MailConfig.java
@@ -8,21 +8,6 @@ import org.springframework.web.client.RestTemplate;
 public class MailConfig {
 
     @Bean
-    public JavaMailSender javaMailSender() {
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("keerthudarshu06@gmail.com");
-        mailSender.setPassword("ifltvezydwelixnc");
-
-        Properties props = mailSender.getJavaMailProperties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-
-        return mailSender;
-    }
-
-    @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
